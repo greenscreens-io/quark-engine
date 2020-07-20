5c51fc232cc3658dec745b9d9bf70ba3e7aec5e6
unknown <greenscreens.ltd@gmail.com>
2020-07-20 09:52:18 +0200

    Initial commit

--- demo/src/main/webapp/js/socket.js
+++ demo/src/main/webapp/js/socket.js
@@ -3,263 +3,277 @@
  */
 
 /**
- * Web and WebSocket API engine 
+ * Web and WebSocket API engine
  * Used to call remote services.
  * All Direct functions linked to io.greenscreens namespace
  */
 SocketChannel = (() => {
-	
-    var tid = 0;
-    var queue = {up : 0, down : 0};    
-    var webSocket = null;
 
-    /**
-     * Update coutners and queue to link resposnes to requests
-     * @param {Object} req
-     *      Request data 
-     */
-    function updateRequest(req, callback) {
-        tid++;
+	var tid = 0;
+	var queue = {
+		up: 0,
+		down: 0
+	};
+	var webSocket = null;
+
+	/**
+	 * Update coutners and queue to link resposnes to requests
+	 * @param {Object} req
+	 *      Request data
+	 */
+	function updateRequest(req, callback) {
+		tid++;
 		req.tid = tid.toString();
-		queue[req.tid] = callback;		
+		queue[req.tid] = callback;
 		queue.up++;
-    }
+	}
 
-    /**
-     * Rerset queue to remove old staleld elements
-     */
-    function cleanQueue() {
-        if (queue.up > 50 && queue.down >= queue.up) {
-			queue = {up : 0, down : 0};
-        }        
-    }
+	/**
+	 * Rerset queue to remove old staleld elements
+	 */
+	function cleanQueue() {
+		if (queue.up > 50 && queue.down >= queue.up) {
+			queue = {
+				up: 0,
+				down: 0
+			};
+		}
+	}
 
-    /**
-     * Check if data can be encrypted
-     * 
-     * @param {Object} req 
-     */
-    function canEncrypt(req) {
-        let hasArgs = Array.isArray(req.data) && req.data.length > 0;
-        return Security.isActive() &&  hasArgs;        
-    }
+	/**
+	 * Check if data can be encrypted
+	 *
+	 * @param {Object} req
+	 */
+	function canEncrypt(req) {
+		let hasArgs = Array.isArray(req.data) && req.data.length > 0;
+		return Security.isActive() && hasArgs;
+	}
 
-    /**
-     * Prepare remtoe call, encrypt if avaialble 
-     * 
-     * @param {Object} req 
-     *         Data to send (optionaly encrypt)
-     */
-    async function onCall(req, callback) {
+	/**
+	 * Prepare remtoe call, encrypt if avaialble
+	 *
+	 * @param {Object} req
+	 *         Data to send (optionaly encrypt)
+	 */
+	async function onCall(req, callback) {
 
-        let enc = null;
-        let data = null;
+		let enc = null;
+		let data = null;
 
-        let isEncrypt = canEncrypt(req);
+		let isEncrypt = canEncrypt(req);
 
-        updateRequest(req, callback);
+		updateRequest(req, callback);
 
-        // encrypt if supported
-        if (isEncrypt) {
+		// encrypt if supported
+		if (isEncrypt) {
 
-            enc = await Security.encrypt(JSON.stringify(req.data));
-            req.data = [enc];
-            data = {cmd:'enc', type:'ws', data : [req]};
+			enc = await Security.encrypt(JSON.stringify(req.data));
+			req.data = [enc];
+			data = {
+				cmd: 'enc',
+				type: 'ws',
+				data: [req]
+			};
 
-        } else {
-             data = {cmd:'data', type:'ws', data : [req]};
-        }
+		} else {
+			data = {
+				cmd: 'data',
+				type: 'ws',
+				data: [req]
+			};
+		}
 
-        // send 
-        webSocket.send(JSON.stringify(data));
+		// send
+		webSocket.send(JSON.stringify(data));
 
-    }    
+	}
 
-    /**
-     * Parse and prepare received message for processing
-     *  
-     * @param {String} mesasge
-     *  
-     */
-    function prepareMessage(message) {
-        
-        let obj = null;
+	/**
+	 * Parse and prepare received message for processing
+	 *
+	 * @param {String} mesasge
+	 *
+	 */
+	function prepareMessage(message) {
 
-        try {
+		let obj = null;
+
+		try {
 
 			if (typeof message === 'string') {
 				obj = JSON.parse(message);
 			}
 
 			if (obj) {
-                onMessage(obj);				
-            } else {
-                Generator.emit('error', event);
-            }
-
-        } catch (e) {
-            Generator.emit('error', e);
-        }
-        
-    }
-
-    /**
-     * Process received message
-     * 
-     * @param {*} msg
-     *  
-     */
-    async function onMessage(obj) {
-
-        let data = null;
-        
-        if (obj.cmd === 'err') {
-            return Generator.emit('error', obj.result);
-        }
-        
-        if (obj.cmd === 'data') {
-            obj = obj.data;
-            doData(obj);
-        } 
-        
-        if (obj.cmd === 'enc') {
-            
-            data = await Security.decrypt(obj);
-            
-            if (data) {
-               doData(data);
-            }
-
-        }
-    }
-
-    /**
-     * Process multiple records in a single response 
-     * 
-     * @param {Object || Array} obj 
-     * 
-     */
-    function doData(obj) {
-        
-        if (Array.isArray(obj)) {
-            
-            obj.every(function(o) {
-                onData(o);
-                return true;
-            });
-            
-		} else {
-
-          onData(obj);
-          	  
-		}				  
-	}
-
-    /**
-     * Process single response record 
-     * 
-     * @param {Object} obj 
-     */
-    function onData(obj) {
-			
-			queue.down++;
-
-			if (typeof queue[obj.tid] === 'function') {
-				try {
-					queue[obj.tid](null, obj);
-				} finally {
-					queue[obj.tid] = null;
-				}
+				onMessage(obj);
+			} else {
+				Generator.emit('error', event);
 			}
 
-            cleanQueue();
-            
-    };    
-        
-    /**
-     * Initialize API call listener
-     */
-    function listener(req, callback) {
-        
-        onCall(req, callback)
-        .catch((e) => {
-           callback(e, null);
-        });
-    }
+		} catch (e) {
+			Generator.emit('error', e);
+		}
+
+	}
 
 	/**
-	 * If wss used in url, create WebSocket channel to 
-	 * exchange API messages 
+	 * Process received message
+	 *
+	 * @param {*} msg
+	 *
+	 */
+	async function onMessage(obj) {
+
+		let data = null;
+
+		if (obj.cmd === 'err') {
+			return Generator.emit('error', obj.result);
+		}
+
+		if (obj.cmd === 'data') {
+			obj = obj.data;
+			doData(obj);
+		}
+
+		if (obj.cmd === 'enc') {
+
+			data = await Security.decrypt(obj);
+
+			if (data) {
+				doData(data);
+			}
+
+		}
+	}
+
+	/**
+	 * Process multiple records in a single response
+	 *
+	 * @param {Object || Array} obj
+	 *
+	 */
+	function doData(obj) {
+
+		if (Array.isArray(obj)) {
+
+			obj.every(function(o) {
+				onData(o);
+				return true;
+			});
+
+		} else {
+
+			onData(obj);
+
+		}
+	}
+
+	/**
+	 * Process single response record
+	 *
+	 * @param {Object} obj
+	 */
+	function onData(obj) {
+
+		queue.down++;
+
+		if (typeof queue[obj.tid] === 'function') {
+			try {
+				queue[obj.tid](null, obj);
+			} finally {
+				queue[obj.tid] = null;
+			}
+		}
+
+		cleanQueue();
+
+	};
+
+	/**
+	 * Initialize API call listener
+	 */
+	function listener(req, callback) {
+
+		onCall(req, callback)
+			.catch((e) => {
+				callback(e, null);
+			});
+	}
+
+	/**
+	 * If wss used in url, create WebSocket channel to
+	 * exchange API messages
 	 */
 	function startSocket(url, resolve, reject) {
-		
+
 		webSocket = new WebSocket(url, ['ws4is']);
-		
-		webSocket.onopen = function (event) {
-            Generator.on('call', listener);			
+
+		webSocket.onopen = function(event) {
+			Generator.on('call', listener);
 			resolve(true);
 		};
-		
-		webSocket.onclose= function (event) {
-            Generator.off('call', listener);
+
+		webSocket.onclose = function(event) {
+			Generator.off('call', listener);
 			webSocket = null;
 		}
-		
-		webSocket.onerror = function (event) {
-            Generator.off('call', listener);
-            reject(event);
-            Generator.emit('error', event);
+
+		webSocket.onerror = function(event) {
+			Generator.off('call', listener);
+			reject(event);
+			Generator.emit('error', event);
 			webSocket = null;
 		};
 
-		webSocket.onmessage = function (event) {        
-            prepareMessage(event.data);            
+		webSocket.onmessage = function(event) {
+			prepareMessage(event.data);
 		};
-						
+
 	}
-      
-    /**
-     * Initialize Socket channel
-     * @param {String} url
-     *      WebSocket Service URL 
-     */
+
+	/**
+	 * Initialize Socket channel
+	 * @param {String} url
+	 *      WebSocket Service URL
+	 */
 	function init(url) {
 
-        kill();
-        
-        return new Promise(function(resolve, reject) {
-            return startSocket(url, resolve, reject);  
+		kill();
+
+		return new Promise(function(resolve, reject) {
+			return startSocket(url, resolve, reject);
 		});
-		
-    }  		
-    
-    /**
-     * Close WebSocket channel if available
-     */
-    function kill() {
-        if (webSocket !== null) {
-            webSocket.close();
-            webSocket = null;
-        }
-    }
-  
-    /**
-     * Exported object with external methods
-     */
-    var exported = {
-	
+
+	}
+
+	/**
+	 * Close WebSocket channel if available
+	 */
+	function kill() {
+		if (webSocket !== null) {
+			webSocket.close();
+			webSocket = null;
+		}
+	}
+
+	/**
+	 * Exported object with external methods
+	 */
+	var exported = {
+
 		init: function(url) {
 			return init(url);
-        },
-        
-        kill : function() {
-            return kill();
-        }
-				
-	};
-  
-    Object.freeze(exported);
+		},
 
-    return exported;
+		kill: function() {
+			return kill();
+		}
+
+	};
+
+	Object.freeze(exported);
+
+	return exported;
 
 })();
--- io.greenscreens.quark/src/javascript/engine.js
+++ io.greenscreens.quark/src/javascript/engine.js
@@ -3,72 +3,72 @@
  */
 
 /**
- * Web and WebSocket API engine 
+ * Web and WebSocket API engine
  * Used to initialize remote API and remote services.
  */
 Engine = (() => {
 
-    const ERROR_MESSAGE = 'Invalid definition for Engine Remote Service';
-      		
+	const ERROR_MESSAGE = 'Invalid definition for Engine Remote Service';
+
 	/**
 	 * Build Js Api from JSON definition retrieved from API service
 	 */
 	async function init(cfg) {
-        
-        // remove all existing listeners
+
+		// remove all existing listeners
 		Generator.off('call');
-        
-        // close socket if used
+
+		// close socket if used
 		if (SocketChannel) {
 			SocketChannel.kill();
-        }
-        
-        cfg = cfg || {};
+		}
 
-        // initialize API
-        let data = await Generator.build(cfg.api);
+		cfg = cfg || {};
 
-        // initialize encryption if provided
-        if (data.signature) {
+		// initialize API
+		let data = await Generator.build(cfg.api);
+
+		// initialize encryption if provided
+		if (data.signature) {
 			if (!Security.isActive()) {
-			   await Security.init(data);
-			}
-        } 
-        
-        // if remote API defined
-		if (cfg.service) {
-
-            // register HTTP/S channel for API
-			if (cfg.service.indexOf('http') === 0) {
-                await WebChannel.init(cfg.service);
-                return true;					
-			}
-            
-            // register WebSocket channel for API
-			if (cfg.service.indexOf('ws') === 0) {
-                await SocketChannel.init(cfg.service);
-                return true;
+				await Security.init(data);
 			}
 		}
-		
-		throw new Error(ERROR_MESSAGE);        
+
+		// if remote API defined
+		if (cfg.service) {
+
+			// register HTTP/S channel for API
+			if (cfg.service.indexOf('http') === 0) {
+				await WebChannel.init(cfg.service);
+				return true;
+			}
+
+			// register WebSocket channel for API
+			if (cfg.service.indexOf('ws') === 0) {
+				await SocketChannel.init(cfg.service);
+				return true;
+			}
+		}
+
+		throw new Error(ERROR_MESSAGE);
 
 	}
-  
-    /**
-     * Exported object with external methods
-     */
-    var exported = {
-	
+
+	/**
+	 * Exported object with external methods
+	 */
+	var exported = {
+
 		init: function(cfg) {
 			return init(cfg);
 		}
-		
-    };
-    
-    Emitter(exported);
-    Object.freeze(exported);
 
-    return exported;
+	};
+
+	Emitter(exported);
+	Object.freeze(exported);
+
+	return exported;
 
 })();
