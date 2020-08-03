a5ac4255354770bb56ee85605971a3ea39519605
unknown <greenscreens.ltd@gmail.com>
2020-08-03 15:12:04 +0200

    JavaScript fix async init for WebSocket

--- demo/src/main/java/io/greenscreens/demo2/SessionProducer.java
+++ demo/src/main/java/io/greenscreens/demo2/SessionProducer.java
@@ -23,46 +23,46 @@ import com.ibm.as400.access.AS400;
 @WebListener
 public class SessionProducer implements ServletRequestListener, HttpSessionListener {
 
-    private static final ThreadLocal<HttpSession> SESSIONS = new ThreadLocal<>();
-    
-    @Override
+	private static final ThreadLocal<HttpSession> SESSIONS = new ThreadLocal<>();
+
+	@Override
 	public void sessionCreated(final HttpSessionEvent se) {}
 
-    /**
-     * We want to ensure to release as400 resources if session is destroyed 
-     */
+	/**
+	* We want to ensure to release as400 resources if session is destroyed
+	*/
 	@Override
 	public void sessionDestroyed(final HttpSessionEvent se) {
 
 		final HttpSession session = se.getSession();
 		final Enumeration<String> keys = session.getAttributeNames();
-		
+
 		while (keys.hasMoreElements()) {
-			
+
 			String key = keys.nextElement();
 			Object obj = session.getAttribute(key);
-		
+
 			if (obj != null && obj instanceof AS400) {
-				((AS400)obj).disconnectAllServices();	
+				((AS400)obj).disconnectAllServices();
 			}
-			
+
 		}
-		
+
 	}
 
 	@Override
-    public void requestDestroyed(final ServletRequestEvent sre) {    	
-        SESSIONS.remove();
-    }
+	public void requestDestroyed(final ServletRequestEvent sre) {
+		SESSIONS.remove();
+	}
 
-    @Override
-    public void requestInitialized(final ServletRequestEvent sre) {
-        SESSIONS.set(HttpServletRequest.class.cast(sre.getServletRequest()).getSession());
-    }
+	@Override
+	public void requestInitialized(final ServletRequestEvent sre) {
+		SESSIONS.set(HttpServletRequest.class.cast(sre.getServletRequest()).getSession());
+	}
 
-    @Produces @Autoinit
-    protected HttpSession getSession() {
-        return SESSIONS.get();
-    }
+	@Produces @Autoinit
+	protected HttpSession getSession() {
+		return SESSIONS.get();
+	}
 
 }
